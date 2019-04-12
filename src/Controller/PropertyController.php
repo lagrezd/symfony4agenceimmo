400922bdb1e84ab17469c7ca19826906271c5c48
Lagreze <lagrezd@gmail.com>
2019-04-12 17:31:24 +0200

    feat: crud property

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 use App\Entity\Property;
 use App\Repository\PropertyRepository;
 use Doctrine\Common\Persistence\ObjectManager;
-use Doctrine\ORM\Repository\RepositoryFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
